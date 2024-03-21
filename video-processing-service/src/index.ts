54deea4235e725e861dfc0481447b56b0448341b
yushanlu <yushanlu1031@gmail.com>
2024-03-21 18:19:55 -0500

    Convert Videos Hosted on GCS

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,40 +1,64 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg"
+import express from 'express';
+
+import { 
+  uploadProcessedVideo,
+  downloadRawVideo,
+  deleteRawVideo,
+  deleteProcessedVideo,
+  convertVideo,
+  setupDirectories
+} from './storage';
+
+// Create the local directories for videos
+setupDirectories();
 
 const app = express();
 app.use(express.json());
 
-// app.get("/", (req, res) => { // when request is made to "/", will res.send
-//     res.send("Hello World");
-// });
+// Process a video file from Cloud Storage into 360p
+app.post('/process-video', async (req, res) => {
 
-app.post("/process-video", (req,res) => {
-    // get path of the input video file from the requst body
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-      // Check if the input file path is defined
-    if (!inputFilePath || !outputFilePath) {
-        return res.status(400).send('Bad Request: Missing file path');
+  // Get the bucket and filename from the Cloud Pub/Sub message
+  let data;
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+    data = JSON.parse(message);
+    if (!data.name) {
+      throw new Error('Invalid message payload received.');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send('Bad Request: missing filename.');
   }
 
-  ffmpeg(inputFilePath)
-    .outputOptions('-vf', 'scale=360:-1') // 360p
-    .on('end', function() {
-        console.log('Processing finished successfully');
-        res.status(200).send('Processing finished successfully');
-    })
-    .on('error', function(err: any) {
-        console.log('An error occurred: ' + err.message);
-        res.status(500).send('An error occurred: ' + err.message);
-    })
-    .save(outputFilePath);
-})
+  const inputFileName = data.name;
+  const outputFileName = `processed-${inputFileName}`;
 
-// app.listen is like opening the doors of your restaurant at a 
-// specific time and announcing, "We are open for business at this location!" In web server terms, app.listen starts the Node.js HTTP server at a specific port
-// It tells your Express application to start listening for incoming requests on that port,
-// making your web server operational and accessible to clients.
+  // Download the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName);
+
+  // Process the video into 360p
+  try { 
+    await convertVideo(inputFileName, outputFileName)
+  } catch (err) {
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ]);
+    return res.status(500).send('Processing failed');
+  }
+  
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName);
+
+  await Promise.all([
+    deleteRawVideo(inputFileName),
+    deleteProcessedVideo(outputFileName)
+  ]);
+
+  return res.status(200).send('Processing finished successfully');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
