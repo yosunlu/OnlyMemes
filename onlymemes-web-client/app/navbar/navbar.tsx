c4562fc1dda56a5068001426362f368e97d8116b
yushanlu <yushanlu1031@gmail.com>
2024-03-22 03:51:53 -0500

    Add sign in with Google

--- onlymemes-web-client/app/navbar/navbar.tsx
+++ onlymemes-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,40 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./navbar.module.css";
+'use client';
 
-export default function Navbar() {
+import SignIn from "./signIn";
+import Link from "next/link";
+
+import styles from "./navbar.module.css";
+// import Upload from "./upload";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
+
+function NavBar() {
+    // Initialize user state
+    const [user, setUser] = useState<User | null>(null);
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChangedHelper((user) => {
+        setUser(user);
+      });
+  
+      // Cleanup subscription on unmount
+      return () => unsubscribe();
+    }, [] /* No dependencies, never rerun */);
+  
+  
     return (
-        <nav className="(styles)">
-            <Link href="/">
-                <Image width={120} height={120}
-                    src="/pngegg.png" alt="Logo"/>              
-            </Link>
-        </nav>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <span className={styles.logoContainer}>
+            <img className={styles.logo} width={100} height={110}
+             src="/pngegg.png" alt="Logo"/>
+          </span>
+        </Link>
+        <SignIn user={user} />
+      </nav>
     );
-}
+  }
+  
+  export default NavBar;
