557b77e7c695ac4e5eff2d3d7ced6381c46d937f
yushanlu <yushanlu1031@gmail.com>
2024-03-23 00:20:53 -0500

    Add upload video function

--- onlymemes-api-service/functions/src/index.ts
+++ onlymemes-api-service/functions/src/index.ts
@@ -2,10 +2,14 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
 
 initializeApp();
 // locally已經firebase login, 所以deploy的時候，已經有自己的credential
-
+const storage = new Storage();
+const rawVideoBucketName = "nc-yt-raw-videos";
 const firestore = new Firestore();
 
 export const createUser = functions.auth.user().onCreate((user) => {
@@ -20,4 +24,31 @@ export const createUser = functions.auth.user().onCreate((user) => {
   // 如果colleciton跟doc本來不存在，會創建
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+// required because our bucket is private, so we need to
+// authenticate the user before they can upload a video.
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+  return {url, fileName};
 });
